53d2db2ab5c782318eecaf9fe373ad9be40b24bb
Alexnander Nasokin <alexnas2python@gmail.com>
2023-03-21 22:59:55 +0300

    Insert and configure footer details

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
 import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
 </script>
 
 <template>
-  <!-- <div class="flex h-screen bg-gray-200 font-roboto"> -->
-  <!-- <div class="flex flex-col overflow-hidden"> -->
-  <AppHeader />
+  <div class="flex flex-col h-screen bg-gray-200 font-roboto">
+    <AppHeader />
 
-  <main class="flex flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
-    <h2>Sidebar</h2>
-    <div class="container mx-auto px-6 py-8">
-      <slot />
-    </div>
-  </main>
-  <h2>Footer</h2>
-  <!-- </div> -->
-  <!-- </div> -->
+    <main class="flex flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+      <h2>Sidebar</h2>
+      <div class="container mx-auto px-6 py-8">
+        <slot />
+      </div>
+    </main>
+    <AppFooter />
+  </div>
 </template>
 
 <style scoped></style>
