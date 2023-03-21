5425ec00dd39fe5e8efe620965c76f5286366070
Alexnander Nasokin <alexnas2python@gmail.com>
2023-03-21 11:27:45 +0300

    Clean

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>The Home Page</h2>
 </template>
