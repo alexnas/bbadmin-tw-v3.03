019c9b13cdb646b2a2366caf269a25315aacc15a
Alexnander Nasokin <alexnas2python@gmail.com>
2023-03-23 10:44:13 +0300

    Make Sidebar Menu dynamic and separate it into specific component

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import SidebarMenu from '@/components/SidebarMenu.vue'
+</script>
+
 <template>
   <div class="flex flex-col w-56 min-w-[theme('spacing[56]')] bg-gray-600">
     <div class="flex items-center justify-start mx-4 my-4">
@@ -6,44 +10,6 @@
       </div>
     </div>
 
-    <div class="overflow-y-auto">
-      <nav class="mt-2">
-        <router-link
-          class="flex items-center px-4 py-2 ml-1 border-l-4 whitespace-nowrap"
-          :class="[$route.name === 'Home' ? activeClass : inactiveClass]"
-          to="/"
-        >
-          <Icon
-            class="w-5 min-w-[theme('spacing[5]')] text-3xl text-red-500"
-            icon="bi:app"
-            :inline="true"
-          />
-          <span class="mx-4">Home</span>
-        </router-link>
-
-        <router-link
-          class="flex items-center px-4 py-2 ml-1 border-l-4 whitespace-nowrap"
-          :class="[$route.name === 'About' ? activeClass : inactiveClass]"
-          to="/about"
-        >
-          <Icon
-            class="w-5 min-w-[theme('spacing[5]')] text-3xl text-red-500 hover:text-orange-400"
-            icon="bi:app"
-            :inline="true"
-          />
-          <span class="mx-4">About</span>
-        </router-link>
-      </nav>
-
-      <div class="text-white px-4 mt-6"></div>
-    </div>
+    <sidebar-menu />
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-import { Icon } from '@iconify/vue'
-
-const activeClass = ref('border-orange-400 text-orange-400 ')
-const inactiveClass = ref('border-gray-900 text-gray-400 hover:text-gray-100')
-</script>
---
+++ src/components/SidebarMenu.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import { Icon } from '@iconify/vue'
+
+interface ISideMenuItem {
+  name: string
+  label: string
+  path: string
+}
+
+let sideMenu: ISideMenuItem[] = reactive([
+  {
+    name: 'Home',
+    label: 'Home',
+    path: '/'
+  },
+  {
+    name: 'About',
+    label: 'About',
+    path: '/about'
+  }
+])
+
+const activeClass = ref('border-orange-400 text-orange-400 ')
+const inactiveClass = ref('border-gray-900 text-gray-400 hover:text-gray-100')
+</script>
+
+<template>
+  <div class="overflow-y-auto">
+    <nav class="mt-2" v-for="item in sideMenu" :key="`${item.name}`">
+      <router-link
+        class="flex items-center px-4 py-2 ml-1 border-l-4 whitespace-nowrap"
+        :class="[$route.name === item.name ? activeClass : inactiveClass]"
+        :to="item.path"
+      >
+        <Icon
+          class="w-5 min-w-[theme('spacing[5]')] text-3xl text-red-500 hover:text-orange-400"
+          icon="bi:app"
+          :inline="true"
+        />
+        <span class="mx-4">{{ item.label }}</span>
+      </router-link>
+    </nav>
+  </div>
+</template>
