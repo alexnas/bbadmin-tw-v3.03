7c29e5ebe956880f5abf454667bb1f1a0821dd43
Alexnander Nasokin <alexnas2python@gmail.com>
2023-03-23 12:43:51 +0300

    Edig Home and About sample pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h2>The Home Page</h2>
+  <div class="mx-auto px-2 mb-4 sm:px-6 lg:px-8 flex flex-col overflow-y-auto">
+    <h1 class="text-2xl font-semibold text-gray-700 my-4">Go By Bus here with us!</h1>
+    <p class="text-base text-justify mb-2">
+      Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat.
+      Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit,
+      eget tincidunt nibh pulvinar a. Curabitur arcu erat, accumsan id imperdiet et, porttitor at
+      sem. Nulla quis lorem ut libero malesuada feugiat. Vestibulum ac diam sit amet quam vehicula
+      elementum sed sit amet dui.
+    </p>
+    <p class="text-base text-justify mb-2">
+      Nulla porttitor accumsan tincidunt. Sed porttitor lectus nibh. Curabitur non nulla sit amet
+      nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat.
+      Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Curabitur aliquet quam id dui
+      posuere blandit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+      Pellentesque in ipsum id orci porta dapibus.
+    </p>
+  </div>
 </template>
