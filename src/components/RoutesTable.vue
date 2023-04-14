a71904d01255fa18485d41bf4e0c39d1cb1ddfae
Alexnander Nasokin <alexnas2python@gmail.com>
2023-04-14 10:50:57 +0300

    Make basic structure for Routes - type, routing, side menu, components

---
+++ src/components/RoutesTable.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>RoutesTable</h1>
+</template>
--- src/types.ts
+++ src/types.ts
@@ -26,4 +26,16 @@ interface ICompany {
   updatedAt: string
 }
 
+interface IRoute {
+  id: number
+  name: string
+  start_time: string
+  end_time: string
+  price: number
+  distance: number
+  description: string
+  createdAt: string
+  updatedAt: string
+}
+
 export type { IProvince, ICity, ICompany }
