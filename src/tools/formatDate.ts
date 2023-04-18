f587b5833ecbc48d84afcd17afac96bc8558dd54
Alexnander Nasokin <alexnas2python@gmail.com>
2023-04-18 12:43:55 +0300

    Refactor RouteForm; change time input format and validation

--- src/tools/formatDate.ts
+++ src/tools/formatDate.ts
@@ -2,7 +2,7 @@ import { format } from 'fecha'
 
 export const formatDateTime = (dateString: string) => {
   try {
-    return format(new Date(dateString), 'YYYY-MM-DD HH:mm')
+    return format(new Date(dateString), 'YYYY/MM/DD, HH:mm')
   } catch (error) {
     console.log(error)
   }
