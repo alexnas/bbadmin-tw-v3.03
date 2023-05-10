0bb4bd535fb51015041577b688b588bf0b010fb9
Alexnander Nasokin <alexnas2python@gmail.com>
2023-05-10 14:09:29 +0300

    Fix reloading pages correctly for logged user

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
-import { useAuthStore } from '@/stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,8 +7,8 @@ const router = createRouter({
 })
 
 router.beforeEach((to) => {
-  const authStore = useAuthStore()
-  if (to.meta.authRequired && !authStore.isAuth) return '/login'
+  const isAuth = localStorage.getItem('token')
+  if (to.meta.authRequired && !isAuth) return '/login'
 })
 
 export default router
