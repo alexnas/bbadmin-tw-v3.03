1e7314ecd1cedc8a5669c04a8a67e97cfc7149eb
Alexnander Nasokin <alexnas2python@gmail.com>
2023-05-10 22:30:34 +0300

    Fix open-close mobile header menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,10 +13,15 @@ const { roles } = storeToRefs(roleStore)
 const router = useRouter()
 const authStore = useAuthStore()
 const { isAuth, loggedUser } = storeToRefs(authStore)
+const isOpen = ref(false)
 
 const roleNameFromId = computed(() => {
   return useItemNameById(loggedUser.value.roleId, roles.value)
 })
+
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value
+}
 
 const handleAuthUser = () => {
   if (isAuth) {
@@ -59,6 +64,7 @@ const inactiveClass = ref(inactiveMenuItem)
             class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
             aria-controls="mobile-menu"
             aria-expanded="false"
+            @click.prevent="toggleMenu"
           >
             <span class="sr-only">Open main menu</span>
             <!--Icon when menu is closed.
@@ -102,7 +108,7 @@ const inactiveClass = ref(inactiveMenuItem)
               />
             </router-link>
           </div>
-          <div class="hidden sm:ml-6 sm:block">
+          <div class="sm:ml-6 sm:block">
             <div class="flex space-x-4">
               <router-link
                 to="/"
@@ -169,15 +175,17 @@ const inactiveClass = ref(inactiveMenuItem)
     </div>
 
     <!-- Mobile menu, show/hide based on menu state. -->
-    <div v-for="item in headerMenu" :key="`${item.name}`" class="sm:hidden" id="mobile-menu">
-      <div class="space-y-1 px-2 pt-2 pb-3">
-        <router-link
-          :to="item.path"
-          class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-          :class="[$route.name === item.name ? activeClass : inactiveClass]"
-          aria-current="page"
-          >{{ item.label }}</router-link
-        >
+    <div v-if="isOpen" class="sm:hidden absolute bg-gray-700 px-3 py-5 rounded-br-lg">
+      <div v-for="item in headerMenu" :key="`${item.name}`" class="sm:hidden" id="mobile-menu">
+        <div class="space-y-1 px-2">
+          <router-link
+            :to="item.path"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+            :class="[$route.name === item.name ? activeClass : inactiveClass]"
+            aria-current="page"
+            >{{ item.label }}</router-link
+          >
+        </div>
       </div>
     </div>
   </nav>
